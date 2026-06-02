4e6d1acc9ccc8ecdfc6be3d89d5ebd0cb981aeda
Saulo Rangel <saulorangel10@hotmail.com>
2026-06-02 07:28:37 -0300

    mural com interações a uma API

---
+++ js/main.js
@@ -0,0 +1,36 @@
+import ui from "./ui.js";
+import api from "./api.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    ui.renderPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    const botaoCancelar = document.getElementById("botao-cancelar")
+
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+    botaoCancelar.addEventListener("click", manipularCancelamento)
+})
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        if (id) {
+            await api.editarPensamento({id, conteudo, autoria })
+        } else {
+        await api.salvarPensamento({conteudo, autoria })
+        }
+        ui.renderPensamentos()
+    }
+    catch (error) {
+        alert("Erro ao salvar o pensamento!")
+    }
+    
+}
+
+function manipularCancelamento() {
+    ui.limparFormulario()
+}
